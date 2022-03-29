78a4c42ce330472789d9514d259c07e38bda81fa
Patrick Dolan <dolanp1992@gmail.com>
2022-03-28 19:01:41 -0700

    add Recipe model

--- RecipeBox/Models/Recipe.cs
+++ RecipeBox/Models/Recipe.cs
@@ -2,6 +2,17 @@ namespace RecipeBox.Models
 {
   public class Recipe
   {
-    
+    private readonly RecipeBoxContext _db;
+
+    public Recipe()
+    {
+      this.JoinEntities = new HashSet<RecipeTag>();
+    }
+
+    public int RecipeId { get; set; }
+    public string Name { get; set; }
+    public string Ingredients { get; set; }
+    public string Instructions { get; set; }
+    public virtual ICollection<RecipeTag> JoinEntities { get; set; }
   }
 }
