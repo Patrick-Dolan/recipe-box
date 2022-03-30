9134ebb09f6baeb01b434a07d9c2000e76e975e8
Patrick Dolan <dolanp1992@gmail.com>
2022-03-29 19:35:28 -0700

    add create and read functionality to tag system
    Co-authored-by: Brit Wallace <Britwallace888@gmail.com>

---
+++ RecipeBox/Controllers/TagsController.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using RecipeBox.Models;
+using System.Linq;
+
+namespace RecipeBox.Controllers
+{
+  public class TagsController : Controller
+  {
+    private readonly RecipeBoxContext _db;
+    public TagsController(RecipeBoxContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      return View(_db.Tags.ToList());
+    }
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+    
+    [HttpPost]
+    public ActionResult Create(Tag tag, int RecipeId)
+    {
+      _db.Tags.Add(tag);
+      if (RecipeId != 0)
+      {
+        _db.RecipeTags.Add(new RecipeTag{ TagId = tag.TagId, RecipeId = RecipeId});
+      }
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+  }
+}
